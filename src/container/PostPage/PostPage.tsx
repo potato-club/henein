1fb32380d223782dde3bc5a381e4517472c563ef
donggyun <donggyunkim12@gmail.com>
2023-02-02 17:39:42 +0900

    feat. postPage styled-components 작성법 수정 #10

--- src/container/PostPage/PostPage.tsx
+++ src/container/PostPage/PostPage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useRouter } from "next/router";
 import styled from "styled-components";
 import dummy from "../../../dummy/dummy.json";
 import Login from "../../components/Login";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { customColor } from "../../constants/customColor";
 
 const PostPage = () => {
@@ -12,36 +12,36 @@ const PostPage = () => {
 
   return (
     <Layout>
-      <div className='boardContent'>
-        <div className='boardTit'>
+      <BoardContent>
+        <BoardTit>
           <div>
             <h3>{router.query.post}</h3>
           </div>
           <div>
             <Image src='/edit_square.svg' width='20' height='20' alt='' />
           </div>
-        </div>
-        <div className='postList'>
+        </BoardTit>
+        <PostList>
           {dummy.userpost2.map((item) => {
             return (
               <PostItem key={item.postnumber}>
-                <div className='leftside'>
-                  <span className='postnum'>{item.postnumber}</span>
+                <LeftSide>
+                  <PostNum>{item.postnumber}</PostNum>
                   <div>
                     <span>{item.title}</span>
-                    <span className='comment'>{`[${item.comment}]`}</span>
+                    <CommentNum>{`[${item.comment}]`}</CommentNum>
                   </div>
-                </div>
-                <div className='rightside'>
+                </LeftSide>
+                <RightSide>
                   <div>
-                    <span className='nickname'>{item.nickname}</span>
-                    <span className='rank'>{item.rank}</span>
+                    <span>{item.nickname}</span>
+                    <Rank>{item.rank}</Rank>
                   </div>
-                  <div className='setting'>
+                  <ImgSpanSet>
                     <Image src='/schedule.svg' width='16' height='16' alt='' />
                     <span>{item.time}</span>
-                  </div>
-                  <div className='setting'>
+                  </ImgSpanSet>
+                  <ImgSpanSet>
                     <Image
                       src='/visibility.svg'
                       width='16'
@@ -49,21 +49,21 @@ const PostPage = () => {
                       alt=''
                     />
                     <span>{item.view}</span>
-                  </div>
-                </div>
+                  </ImgSpanSet>
+                </RightSide>
               </PostItem>
             );
           })}
-        </div>
-        <div className='moreinfo'>
+        </PostList>
+        <MoreInfo>
           <button>
-            <Image src='/expand_more.svg' width='12' height='7.4' alt='' />
+            <Image src='/expand_more.svg' width='12' height='7' alt='' />
           </button>
-        </div>
-      </div>
-      <div className='aside'>
+        </MoreInfo>
+      </BoardContent>
+      <Aside>
         <Login />
-      </div>
+      </Aside>
     </Layout>
   );
 };
@@ -79,66 +79,62 @@ const Layout = styled.div`
   margin: 0 auto;
   background-color: white;
   box-sizing: border-box;
-  .boardContent {
-    display: flex;
-    flex-direction: column;
-    border: 1px solid #e6e6e6;
-    border-top: none;
-    border-radius: 32px;
-    width: 808px;
-    height: 872px;
-    box-sizing: border-box;
-    z-index: 0.5;
-    .moreinfo {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 20px 24px;
-      height: 64px;
-      border-top: 1px solid #e6e6e6;
-      button {
-        width: 24px;
-        height: 24px;
-        background-color: white;
-        border: none;
-      }
-      button:hover {
-        cursor: pointer;
-      }
-    }
-    .boardTit {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      background-color: white;
-      height: 64px;
-      border-radius: 32px;
-      border: 1px solid #e6e6e6;
-      z-index: 1;
-      box-shadow: 0 2px 3px rgba(255, 128, 56, 0.2);
-      h3 {
-        color: ${customColor.orange};
-        font-weight: bold;
-        margin-left: 24px;
-        font-size: 18px;
-      }
-      img {
-        margin-right: 24px;
-      }
-    }
-    .postList {
-      display: flex;
-      flex-direction: column;
-      margin: 0 24px;
-      margin-top: 12px;
-      margin-bottom: 12px;
-    }
+`;
+const BoardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #e6e6e6;
+  border-top: none;
+  border-radius: 32px;
+  width: 808px;
+  height: 872px;
+  box-sizing: border-box;
+  z-index: 0.5;
+`;
+const BoardTit = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  height: 64px;
+  border-radius: 32px;
+  border: 1px solid #e6e6e6;
+  z-index: 1;
+  box-shadow: 0 2px 3px rgba(255, 128, 56, 0.2);
+  h3 {
+    color: ${customColor.orange};
+    font-weight: bold;
+    margin-left: 24px;
+    font-size: 18px;
   }
+  img {
+    margin-right: 24px;
+  }
+`;
 
-  .aside {
-    display: flex;
-    width: 300px;
+const PostList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 24px;
+  margin-top: 12px;
+  margin-bottom: 12px;
+`;
+
+const MoreInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 24px;
+  height: 64px;
+  border-top: 1px solid #e6e6e6;
+  button {
+    width: 24px;
+    height: 24px;
     background-color: white;
+    border: none;
+  }
+  button:hover {
+    cursor: pointer;
   }
 `;
 
@@ -150,45 +146,55 @@ const PostItem = styled.div`
   height: 36px;
   line-height: 16px;
   gap: 10.5;
+`;
+
+const LeftSide = styled.div`
+  display: flex;
+  gap: 12px;
+  div {
+    display: flex;
+    gap: 4px;
+  }
   span {
     font-size: 12px;
   }
-  div.leftside {
+`;
+const PostNum = styled.span`
+  width: 43px;
+`;
+const CommentNum = styled.span`
+  color: ${customColor.orange};
+`;
+
+const RightSide = styled.div`
+  display: flex;
+  gap: 12px;
+  color: ${customColor.darkGray};
+  div {
     display: flex;
-    gap: 12px;
-    .postnum {
-      width: 43px;
-    }
-    div {
-      display: flex;
-      gap: 4px;
-    }
-    .comment {
-      color: ${customColor.orange};
-    }
+    gap: 4px;
   }
-  div.rightside {
-    display: flex;
-    gap: 12px;
-    color: ${customColor.darkGray};
-    div {
-      display: flex;
-      gap: 4px;
-    }
-    .rank {
-      display: flex;
-      justify-content: center;
-      color: white;
-      background-color: #4b79ef;
-      border-radius: 8px;
-      font-size: 10px;
-      font-weight: 400px;
-      width: 30px;
-      height: 16px;
-    }
-    .setting {
-      display: flex;
-      justify-content: center;
-    }
+  span {
+    font-size: 12px;
   }
+`;
+const Rank = styled.div`
+  display: flex;
+  justify-content: center;
+  color: white;
+  background-color: #4b79ef;
+  border-radius: 8px;
+  width: 30px;
+  height: 16px;
+  font-size: 10px;
+`;
+const ImgSpanSet = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Aside = styled.aside`
+  display: flex;
+  width: 300px;
+  background-color: white;
 `;

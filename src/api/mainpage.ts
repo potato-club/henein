3d31a98fb0de92d7bdc4fdb5b473ba883829bb5d
donggyun <donggyunkim12@gmail.com>
2023-03-06 16:12:07 +0900

    feat. env적용, mainPage UserPostList리팩토링 #6

--- src/api/mainpage.ts
+++ src/api/mainpage.ts
@@ -2,13 +2,17 @@ import axios from "axios";
 
 // free,humor,boss,info
 export const getApi = async (boardName: string) => {
-  const json = await axios.get(`https://henesysback.shop/board/${boardName}`);
+  const json = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/board/${boardName}`
+  );
   const apiData = await json.data;
   return apiData;
 };
 
 export const getEntireBoard = async () => {
-  const json = await axios.get(`https://henesysback.shop/board/entireboard`);
+  const json = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/board/entireboard`
+  );
   const apiData = await json.data;
   return apiData;
 };

a5d51c66ca3e3226ffdee3d26438bd68b8b4a608
donggyun <donggyunkim12@gmail.com>
2023-11-06 23:35:09 +0900

    fix(Footer,Layout): Footer 화면 크기에 맞춰 최하단에 배치

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
-import Footer from './Footer';
-import Header from './Header';
-import { ThemeProvider } from 'styled-components';
-import { lightMode, darkMode } from '../constants/DefaultTheme';
-import GlobalStyles from '../../styles/GlobalStyles';
-import { ScrollProvider } from './ScrollProvider';
+import React, { useEffect, useState } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import styled, { ThemeProvider } from "styled-components";
+import { lightMode, darkMode } from "../constants/DefaultTheme";
+import GlobalStyles from "../../styles/GlobalStyles";
+import { ScrollProvider } from "./ScrollProvider";
 
 const Layout = ({ children }: React.PropsWithChildren) => {
-  const [theme, setTheme] = useState('light');
-
+  const [theme, setTheme] = useState("light");
   return (
-    <ThemeProvider theme={theme === 'light' ? lightMode : darkMode}>
+    <ThemeProvider theme={theme === "light" ? lightMode : darkMode}>
       <ScrollProvider>
         <GlobalStyles />
-        <Header setTheme={setTheme} />
-        {children}
-        <Footer />
+        <PageDiv>
+          <div>
+            <Header setTheme={setTheme} />
+            {children}
+          </div>
+          <Footer />
+        </PageDiv>
       </ScrollProvider>
     </ThemeProvider>
   );
 };
 
 export default Layout;
+
+const PageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+`;

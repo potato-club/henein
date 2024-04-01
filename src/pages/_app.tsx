5a88a11418c770882f80cd657ba041e94a0db31f
Pdom <pdom0327@gmail.com>
2024-04-01 22:28:33 +0900

    feat: 변경된 api 대응

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,28 @@
 import '../../styles/globals.css';
-import type { AppProps } from "next/app";
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import { useState } from "react";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { useRouter } from "next/router";
-import Layout from "../component/Layout";
-import { Provider } from "react-redux";
-import { store, persistor } from "../../store/store";
-import SecondLayout from "../component/SecondLayout";
-import { ThemeProvider } from "next-themes";
-import { PersistGate } from "redux-persist/integration/react";
+import type { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { useRouter } from 'next/router';
+import Layout from '../component/Layout';
+import { Provider } from 'react-redux';
+import { store, persistor } from '../../store/store';
+import SecondLayout from '../component/SecondLayout';
+import { ThemeProvider } from 'next-themes';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = useState(() => new QueryClient())[0];
   const router = useRouter();
-  const shouldRenderLayout = !["/login", "/signUp", "/register"].includes(
+  const shouldRenderLayout = !['/login', '/signUp', '/register'].includes(
     router.pathname
   );
 
   return (
     <>
-      <ThemeProvider>
-        <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
           <Hydrate state={pageProps.dehydratedState}>
             <Provider store={store}>
               <PersistGate persistor={persistor}>
@@ -38,8 +39,8 @@ export default function App({ Component, pageProps }: AppProps) {
             </Provider>
           </Hydrate>
           <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-        </QueryClientProvider>
-      </ThemeProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }

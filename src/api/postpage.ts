cc41279a31d79a1e1853ca95c5862db1d2eda6f0
donggyun <donggyunkim12@gmail.com>
2024-05-08 17:39:47 +0900

    fix: api size 속성 추가에따른 수정

--- src/api/postpage.ts
+++ src/api/postpage.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-export const getEachBoard = async (boardName: string, pageNum: number) => {
+export const getEachBoard = async (
+  boardName: string,
+  pageNum: number,
+  size: number = 20
+) => {
   const json = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/board/?type=${boardName}&page=${pageNum}`
+    `${process.env.NEXT_PUBLIC_API_URL}/board/?type=${boardName}&page=${pageNum}&size=${size}`
   );
   const apiData = await json.data;
   return apiData;

3a220cd5a0300b6a987cdb13c5d483602cc6893a
donggyun <donggyunkim12@gmail.com>
2024-05-08 17:45:48 +0900

    fix: commentInfo 전역변수 slice 분리

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,15 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import darkModeReducer from "./darkmodeSlice/darkmode";
-import onWarningReducer from "./warningSlice/onWarning";
-import userInfoReducer from "./userInfoSlice/userInfo";
-import commentInfoReducer from "./warningSlice/commentInfo";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { combineReducers } from '@reduxjs/toolkit';
+import darkModeReducer from './darkmodeSlice/darkmode';
+import onWarningReducer from './warningSlice/onWarning';
+import userInfoReducer from './userInfoSlice/userInfo';
+import commentInfoReducer from './comment/commentInfo';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
-  key: "root", // localStorage key
+  key: 'root', // localStorage key
   storage, // localStorage
-  whitelist: ["darkMode"], // target (reducer name)
+  whitelist: ['darkMode'], // target (reducer name)
 };
 
 const rootReducer = combineReducers({
